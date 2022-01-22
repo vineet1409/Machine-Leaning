dcab7ff3ffacb66aa317c24a1dfae9dea203d02d
vineet <srivineet93@gmail.com>
2022-01-23 02:15:08 +0530

    updated readme file and static/recommend.js file

--- Content_based_movie_recommendation/static/recommend.js
+++ Content_based_movie_recommendation/static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = 'YOUR_API_KEY';
+    var my_api_key = 'ea3f0abedcda394b13bda59073a255fa'; // enter your API key
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = 'YOUR_API_KEY';
+  var my_api_key = 'ea3f0abedcda394b13bda59073a255fa';   // enter your API key
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
